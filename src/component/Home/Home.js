e3c5c11da51d54d31f28bec8c97a349c15dcf464
mohiuddintanvir <mohiuddin.tanvir.mollah@gmail.com>
2022-12-26 21:20:24 +0600

    second final

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -5,12 +5,12 @@ const Home = () => {
     return ( 
         <div className=' '>
             <div className='mt-20'>
-                <h1 className='text-xl'>async</h1>
-            <p className='text-[80px] font-[Bilbo]'>tic tac</p>
-            <p className='text-[80px]'>toe</p> 
+                <h1 className='text-xl text-[#000000] text-center'>async</h1>
+            <p className='text-[80px] font-[Bilbo] text-center text-[#000000]'>tic tac</p>
+            <p className='text-[80px] text-center text-[#000000]'>toe</p> 
             </div>
            
-            <div className='mt-60'>
+            <div className='mt-20'>
                        <Link to='/login'> <button className="btn btn-warning w-full lg:w-96">Login </button></Link><br />
             <Link to='/register'><button className="btn bg-blue-500 w-full lg:w-96 text-black mt-5">Register</button></Link>    
             </div>
